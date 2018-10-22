a2bb8ca85a400ae33da5a012a9b8ed6eb30354b6
言未尽 <a965781534@163.com>
2018-10-22 10:34:39 +0800

    all code

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -24,20 +25,36 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fm) {
+    private Context context;
+
+    public CategoryAdapter(FragmentManager fm, Context context) {
         super(fm);
+        this.context = context;
     }
 
     @Override
     public Fragment getItem(int position) {
         if (position == 0) {
             return new NumbersFragment();
-        } else if (position == 1){
+        } else if (position == 1) {
             return new FamilyFragment();
-        } else if (position == 2){
+        } else if (position == 2) {
             return new ColorsFragment();
         } else {
             return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return context.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return context.getString(R.string.category_family);
+        } else if (position == 2) {
+            return context.getString(R.string.category_colors);
+        } else {
+            return context.getString(R.string.category_phrases);
         }
     }
 
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -30,9 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager(), MainActivity.this);
 
         viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 
